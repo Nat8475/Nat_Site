c0803b3866db8cf5ab716bb7aeeb5191e2e3e434
Nat1478 <datandarosa@gmail.com>
2024-04-28 16:04:57 -0300

    new file:   .gitignore 	new file:   README.md 	new file:   assets/css/style.css 	new file:   assets/css/style2.css 	new file:   assets/css/style3.css 	new file:   assets/img/back.png 	new file:   assets/img/background.jpg 	new file:   assets/img/background2.svg 	new file:   assets/img/logo.png 	new file:   assets/img/user.png 	new file:   assets/js/index.js 	new file:   assets/js/index2.js 	new file:   assets/js/login.js 	new file:   index.html 	new file:   index2.html 	new file:   login.html

---
+++ assets/js/login.js
@@ -0,0 +1,27 @@
+document.getElementById("button").onclick = function(){
+    Swal.fire({
+        icon: "success",
+        title: "Você logou com Sucesso",
+        //showDenyButton: true,
+        confirmButtonText: "Voltar"
+    })
+    
+    
+    /*
+    Swal.fire({
+        icon: "warning",
+        title: "Você tem certeza?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Salvar",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire("Saved!", "", "success");
+        } else if(result.isDenied) {
+            Swal.fire("As mudanças não foram salvas", "", "info");
+        }
+    })*/
+}
+document.getElementById("email").onclick = function(){
+    this.value = ''
+}
